f873cd33f219f334eb92ff3906b60e863514ac71
Ayber Ulas Keskin <ayberulaskeskin@gmail.com>
2021-05-24 16:22:03 +0300

    boolean experiment

--- js/01-hello-world.js
+++ js/01-hello-world.js
@@ -1,3 +1,51 @@
-document.write("hello world");
-alert("hello world");
-console.log("hello world");
+//document.write("hello world");
+//alert("hello world");
+//console.log("hello world");
+//var serveName="hello world"
+//console.log(serveName);
+///let serverName;
+
+
+//serverName="http://oxymeter.tk"
+
+//console.log(serverName)
+//let pasword ="1234";
+
+//console.log(pasword)
+//pasword="hello hello hello"
+//console.log(pasword)
+//const dizi=[1,2,3]
+//console.log(dizi)
+//dizi=[1,2,1]
+//console.log(dizi)
+let price =100
+let tax=0.18
+let priceTax=price*tax
+let total=price+priceTax
+console.log(
+   "Fiyat:",price,
+   "KDV Oranı:",tax,
+   "Kdv Tutarı:",priceTax,
+   "Fiyat:",total
+   )
+let counter=320
+counter++;
+counter+=10;
+counter*=10;
+counter-=10;
+counter/=10;
+console.log(counter);
+console.log(2+3*10);
+
+console.log(17%2)
+console.log(18%8)
+console.log(2**4)
+console.log(  Math.floor(1.7))//aşağı yuvvarlama
+console.log(Math.ceil(1.1))//yukarı yuvarlama
+console.log(Math.round(1.4))//yakına yuvarlama
+
+
+let stringNumber="11"
+let newnumber=Number(stringNumber)
+console.log(stringNumber,newnumber)
+console.log(Number("111"));
