dfc421a969fd02ebfa1fc3bb6c97743f2874479a
Ayber Ulas Keskin <ayberulaskeskin@gmail.com>
2021-05-26 22:24:43 +0300

    string type experiment

--- js/01-hello-world.js
+++ js/01-hello-world.js
@@ -1,11 +1,20 @@
-/*let isActive=false  
-isActive=true
-console.log(isActive)
-
-
-let userName="user";
-let isUserName=Boolean(userName)
-console.log(isUserName)
-console.log(Boolean(userName.length>0))*/
-var y;
-console.log(Boolean(y))
+let number="11.1"
+number=parseFloat(number)
+console.log("number:",number,typeof(number))
+number=number.toString()
+console.log(number,typeof(number),String(123))
+console.log(''+123)//Implicit Coercion(örtülü dönüşün)
+console.log(String(123))//Explicit Coercion (aşçık dönüşüm)
+console.log(['a']+[9,2,3])
+console.log(12/'2')
+console.log(NaN===NaN)
+console.log("foo" + +"bar")// +"bar" => Number("bar") => NaN //"foo"+NaN //"foo" +"NaN" //Sonuç= "fooNaN"
+console.log(1 == true)
+console.log(null=="")// null sadece kendine ve undefined'a eşit olduğundan //Sonuç= false
+let userName="jupi"
+let domain="jupi.com"
+let info=`merhaba ${userName} 
+mail adersiniz ${domain}
+${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}
+`
+console.log(info)
