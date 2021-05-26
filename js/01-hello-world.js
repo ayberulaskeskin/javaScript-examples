6e08df498529e94633f7b3291e41e1a40caf1c99
Ayber Ulas Keskin <ayberulaskeskin@gmail.com>
2021-05-26 22:42:58 +0300

    introduction to DOM

--- js/01-hello-world.js
+++ js/01-hello-world.js
@@ -5,16 +5,27 @@ number=number.toString()
 console.log(number,typeof(number),String(123))
 console.log(''+123)//Implicit Coercion(örtülü dönüşün)
 console.log(String(123))//Explicit Coercion (aşçık dönüşüm)
-console.log(['a']+[9,2,3])
+console.log([2,3,5]+[9,2,3])
 console.log(12/'2')
 console.log(NaN===NaN)
 console.log("foo" + +"bar")// +"bar" => Number("bar") => NaN //"foo"+NaN //"foo" +"NaN" //Sonuç= "fooNaN"
 console.log(1 == true)
 console.log(null=="")// null sadece kendine ve undefined'a eşit olduğundan //Sonuç= false
 let userName="jupi"
-let domain="jupi.com"
+let email="jupi@gmail.com"
 let info=`merhaba ${userName} 
-mail adersiniz ${domain}
+mail adersiniz ${email}
 ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}
 `
 console.log(info)
+console.log(email.search('@'))
+console.log(email[4]) 
+let domain =email.slice( email.search("@")+1)
+console.log(domain)
+console.log(domain.indexOf("."))
+email=email.replace("gmail.com","kodluyoruz.org")
+console.log(email)
+console.log(email.endsWith("kodluyoruz.com"))
+userName=`${userName[0].toUpperCase()}${userName.slice(1)}`
+console.log(userName)
+console.log(userName.charCodeAt(0))
