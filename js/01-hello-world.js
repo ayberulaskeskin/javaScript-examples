82a8e4c0fe4edebf750224ea8bdeaf2bf92bbc72
Ayber Ulas Keskin <ayberulaskeskin@gmail.com>
2021-05-27 17:56:39 +0300

    add remove or replace class information

--- js/01-hello-world.js
+++ js/01-hello-world.js
@@ -1,5 +1,18 @@
 
 
-let fullName =prompt("Adınınızı giriniz")       
- let title =document.querySelector("#title")
- title.innerHTML=`${title.innerHTML}<small style="color:green">${fullName}</small>`
+let lastChild = document.querySelector("ul#list>li:last-child")
+lastChild.innerHTML="değiştirdim"
+lastChild.innerHTML+=" ekledim "
+
+let firstChild=document.querySelector("ul#list>li:first-child")
+firstChild.innerHTML="değiştirdim"
+firstChild.innerHTML+=" ekledim "
+//let secondChild=document.querySelector("ul#list>li:third-child")
+//secondChild.innerHTML="değiştirdim"
+//secondChild.innerHTML+=" ekledim "
+
+let ulDOM=document.querySelector("ul#list")
+let liDOM=document.createElement("li")
+liDOM.innerHTML="yeni oluşturduğum"
+ulDOM.appendChild(liDOM)
+ulDOM.prepend(liDOM)
